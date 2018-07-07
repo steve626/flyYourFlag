f058a1c5cddb4ec4628858fce7cfccdcde273d9b
Steve Valasek <svalasek@gmail.com>
2018-07-07 13:23:59 -0700

    added backend and testing

---
+++ controllers/user_controllers.js
@@ -0,0 +1,57 @@
+const user = require('../models/users')
+
+module.exports = {
+  greeting(req, res) {
+    res.send({ hi: 'there' });
+  },
+
+  index(req, res, next) {
+    const { lng, lat } = req.query;
+    const point = {
+      type: 'Point',
+      coordinates: [parseFloat(lng), parseFloat(lat)]
+    };
+    user.aggregate([
+      {
+        $geoNear: {
+          near: point,
+          spherical: true,
+          maxDistance: 200000,
+          distanceField: 'dist.calculated'
+        }
+      }
+    ])
+    .then((users) => {
+      res.send(users);
+    })
+    .catch(next);
+  },
+
+  create(req, res, next) {
+    const userProps = req.body;
+
+    user.create(userProps)
+      .then(user => res.send(user))
+      .catch(next);
+    //console.log(req.body);
+    
+  },
+
+  edit(req,res,next) {
+    const userId = req.params.id;
+    const userProps = req.body;
+
+    user.findByIdAndUpdate({ _id: userId }, userProps)
+      .then(() => user.findById({ _id: userId }))
+      .then(user => res.send(user))
+      .catch(next);
+  },
+
+  delete(req, res, next) {
+    const userId = req.params.id;
+    
+    user.findByIdAndRemove({ _id: userId })
+    .then( user => res.status(204).send(user))
+    .catch(next);
+  }
+};
