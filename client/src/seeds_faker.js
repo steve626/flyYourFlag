4e155692c94425038583872bdaac057daa045c2a
Steve Valasek <svalasek@gmail.com>
2018-07-08 17:00:50 -0700

    added faker user template

---
+++ client/src/seeds_faker.js
@@ -0,0 +1,42 @@
+import _ from 'lodash';
+import faker from 'faker';
+import { Db, Server } from 'mongodb';
+
+const faker = require('faker/locale/en-US');
+
+const MINIMUM_USERS = 2000;
+const USERS_TO_ADD = 15000;
+
+let usersCollection;
+const db = new Db('fly_your_flag', new Server('localhost', 27017));
+db.open()
+.then(() => {
+  usersCollection = db.collection('users');
+  return usersCollection.count({});
+})
+.then(count => {
+  if (count < MINIMUM_USERS){
+    const users = _.times(USERS_TO_ADD, () => createUsers());
+
+    usersCollection.insertMany(users);
+  }
+})
+.catch(err => console.log(err));
+
+
+function createUsers() {
+  return {
+    email: faker.email.findEmail(),
+    password: faker.password.findPassword(),
+    //assign team to _id of teams in teamDB should be ~100 fans of each team
+    team: randomBetween(0, 148),
+    geometry: {
+      lng: faker.lng.randomBetween(-124, -70),
+      lat: faker.lat.randomBetween(25, 49)
+    }
+  };
+}
+
+function randomBetween(min, max) {
+  return ~~(Math.random() * (max-min)) + min;
+}
