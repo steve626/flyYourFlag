075225745c0cab7bb998dd2d1094996756061585
Maverick <embrymaverick@gmail.com>
2018-07-11 16:30:20 -0700

    Added body parser

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 //const routes = require("./routes");
 const app = express();
 
+const bodyParser = require("body-parser");
+
 const routes = require("./routes");
 
 const PORT = process.env.PORT || 3001;
