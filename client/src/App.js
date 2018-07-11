df38351d04dd80d8250953f02f5117e0fe0c0c84
Steve Valasek <svalasek@gmail.com>
2018-07-11 16:08:53 -0700

    require react-router-dom, still broke

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 // import LogIn from './pages/LogIn';
 // import MapView from './pages/MapView';
