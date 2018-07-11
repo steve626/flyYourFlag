00df07ce753f243367a00ebdfda3101532c438d8
Steve Valasek <svalasek@gmail.com>
2018-07-10 20:35:53 -0700

    did what Maverick told me to do with app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,25 @@
 import React, { Component, Fragment } from 'react';
-import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import LogIn from './pages/LogIn';
-import MapView from './pages/MapView';
-import Profile from './pages/Profile';
-import TeamChooser from './pages/TeamChooser';
-import NoMatch from './pages/NoMatch';
-import Nav from './components/Nav';
+
+
+// import LogIn from './pages/LogIn';
+// import MapView from './pages/MapView';
+// import Profile from './pages/Profile';
+// import TeamChooser from './pages/TeamChooser';
+// import NoMatch from './pages/NoMatch';
+// import Nav from './components/Nav';
+import View from './components/View';
 
 const App = () => (
   <Router>
     <div>
-      <Nav />
+      
       <Switch>
-        <Route exact path = '/' component = {LogIn} />
+        <Route exact path = '/view' component = {View} />
+        {/* <Route exact path = '/' component = {LogIn} />
         <Route exact path = '/MapView' component = {MapView} />
         <Route exact path = '/Profile' component = {Profile} />
         <Route exact path = '/TeamChooser' component = {TeamChooser} />
-        <Route exact path = '/NoMatch' component = {NoMatch} />
+        <Route exact path = '/NoMatch' component = {NoMatch} /> */}
       </Switch>
     </div>
   </Router>
